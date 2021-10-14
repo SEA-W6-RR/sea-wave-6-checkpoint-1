deb495d2155a827edf1dcfcce9def5aba6f52397
SEA-W6-RR <90201728+SEA-W6-RR@users.noreply.github.com>
2021-10-14 12:56:44 +0200

    Repairing Names => done

--- src/main/java/dev/wilders/ITPersonRepair.java
+++ src/main/java/dev/wilders/ITPersonRepair.java
@@ -5,6 +5,13 @@ public class ITPersonRepair {
     public static String repairName(ITPerson person) {
         // Get the name of the ITPerson, repair it and return the new name
         // EG: person.getName() -> "Super_Admin" -> return "Super Admin"
-        return "";
+
+        ITPerson corruptedName = person;
+        // System.out.println(corruptedName.getName());
+        String[] temp = corruptedName.getName().split("_");
+        String firstPart = temp[0];
+        String secondPart = temp[1];
+        // System.out.println(firstPart + " " + secondPart);
+        return (firstPart + " " + secondPart);
     }
 }
